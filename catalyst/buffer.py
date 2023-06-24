904d08d48e8eba12fdf29c3ac6f86c25ee30e63a
Valentin B <valentin.be@protonmail.com>
2023-06-25 01:52:23 +0200

    Fix frame buffering logic

--- catalyst/buffer.py
+++ catalyst/buffer.py
@@ -17,6 +17,9 @@ class PacketBuffer:
     def __init__(self):
         self._buf = bytearray()
 
+        self._encrypted = False
+        self._header = None
+
     def add_packet(self, data: bytes):
         self._buf.extend(data)
 
@@ -24,20 +27,23 @@ class PacketBuffer:
         if len(self._buf) < 8:
             return None
 
-        encrypted = not (ctx is None or is_plaintext_frame(self._buf))
+        # Check if we already started breaking down a frame. If not,
+        # consume its header and determine if it's encrypted.
+        if self._header is None:
+            self._encrypted = not (ctx is None or is_plaintext_frame(self._buf))
+            self._header = self._buf[:8]
 
-        # Before we start reading, make sure the first bytes of
-        # an encrypted frame are in plaintext. Every frame has
-        # at least 8 bytes, so this is good enough for us.
-        if encrypted:
-            self._buf[0:8] = ctx.decrypt(self._buf[0:8])  # type:ignore
+            if self._encrypted:
+                self._header = ctx.decrypt(self._header)  # type:ignore
 
-        reader = ByteInterface(self._buf)
+        reader = ByteInterface(self._header)
 
+        # Read the header magic and validate it.
         magic = reader.unsigned2()
         if magic != 0xF00D:
             raise ValueError("received unsupported frame data")
 
+        # Read the frame size to determine if we have enough data.
         size = reader.unsigned2() + 4
         if size >= 0x8000:
             size = reader.unsigned4() + 8
@@ -48,10 +54,11 @@ class PacketBuffer:
 
         # Extract the frame and decrypt it, if necessary.
         frame = self._buf[:size]
-        if encrypted:
-            frame = self._buf[:8] + ctx.decrypt(frame[8:])  # type:ignore
+        if self._encrypted:
+            frame = self._header + ctx.decrypt(frame[8:])  # type:ignore
 
         # Keep the remainder for the next round of processing.
         self._buf = self._buf[size:]
+        self._header = None
 
-        return encrypted, frame
+        return self._encrypted, frame
