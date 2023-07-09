e0c3aaa57f57d247de19d35079318e7debf4b709
Valentin B <valentin.be@protonmail.com>
2023-07-09 02:39:22 +0200

    Deduplicate some code in ScapyPlugin

--- wizproxy/plugin/scapy.py
+++ wizproxy/plugin/scapy.py
@@ -4,7 +4,7 @@ import trio
 from scapy.layers.inet import Ether, IP, TCP
 from scapy.utils import PcapNgWriter
 
-from ..proto import Bytes, Frame
+from ..proto import Bytes, Frame, SocketAddress
 from . import Context, Direction, Plugin, listen
 
 
@@ -29,10 +29,17 @@ class ScapyPlugin(Plugin):
         writer = PcapNgWriter(str(path.resolve()))
         return cls(writer)
 
-    async def write_to_file(self, ctx: Context, tcp: TCP, ip: IP, raw: bytes):
+    async def write_to_file(
+        self, ctx: Context, source: SocketAddress, dest: SocketAddress, raw: bytes
+    ):
         shard = ctx.shard()
 
-        packet = Ether() / ip / tcp / raw
+        packet = (
+            Ether()
+            / IP(src=source.ip, dst=dest.ip)
+            / TCP(sport=source.port, dport=dest.port)
+            / raw
+        )
         packet.comment = f"Shard {shard.ip}:{shard.port}, client {ctx.session.sid}"
 
         await trio.to_thread.run_sync(self.writer.write, packet)
@@ -42,15 +49,13 @@ class ScapyPlugin(Plugin):
         bytes = Bytes()
         frame.write(bytes)
 
-        ip = IP(src=ctx.session.client.ip, dst=ctx.session.server.ip)
-        tcp = TCP(sport=ctx.session.client.port, dport=ctx.session.server.port)
-        await self.write_to_file(ctx, tcp, ip, bytes.getvalue())
+        session = ctx.session
+        await self.write_to_file(ctx, session.client, session.server, bytes.getvalue())
 
     @listen(Direction.SERVER_TO_CLIENT)
     async def serverbound(self, ctx: Context, frame: Frame):
         bytes = Bytes()
         frame.write(bytes)
 
-        ip = IP(src=ctx.session.server.ip, dst=ctx.session.client.ip)
-        tcp = TCP(sport=ctx.session.server.port, dport=ctx.session.client.port)
-        await self.write_to_file(ctx, tcp, ip, bytes.getvalue())
+        session = ctx.session
+        await self.write_to_file(ctx, session.server, session.client, bytes.getvalue())
